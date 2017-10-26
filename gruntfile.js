a3d3009d396ce160964bedcb112ad0e77cb3ed4a
RobertK <robertkuczynski@gmail.com>
2017-10-26 02:20:16 +0200

    dodany grunt i css

---
+++ gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    jshint: {
+      all: ['js/*.js']
+    },
+
+    sass: {
+      options: {
+        sourceMap: true
+      },
+      dist: {
+        files: {
+          'css/style.css': 'css/input.scss'
+        }
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: ['css/*.scss'],
+        tasks: ['sass'],
+        options: {
+            spawn: false,
+          }
+      }
+    },
+
+
+  });
+
+
+
+  // Load the plugins tasks
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+
+
+  // Default task(s).
+  grunt.registerTask('default', ["jshint", "sass", "watch"]);
+};
