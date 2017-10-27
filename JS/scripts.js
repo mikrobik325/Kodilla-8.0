d4276dec7655df18949e3d94d4cd053943ba5cb4
Robert Kuczyński <robertkuczynski@gmail.com>
2017-10-27 10:59:30 +0200

    dodany koment do scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,15 @@
 // scripts.js file
 
+// wyświetla okno z polem do uzupełnienia
+
 var name = prompt('Enter your name');
 alert('Hello, ' + name);
 console.log('Hello, ' + name);
+
+
+//to ponieżej to nie wiem co ma robić
+
+/*
+var variable = 'test';
+console.log(variable);
+*/
